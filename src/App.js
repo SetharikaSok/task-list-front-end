42cbe0830ce3272a1d7b35bf59408eac66efab93
Setharika Sok <soksetharika7@gmail.com>
2023-06-22 16:33:46 -0400

    Calling task_list_api from backend project

--- src/App.js
+++ src/App.js
@@ -1,39 +1,74 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
 
-const taskData= [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
 
 const App = () => {
-  const [tasks, setTasks] = React.useState(taskData);
+  const [tasks, setTasks] = React.useState([]);
 
-  const setComplete = (id) => (
-    setTasks(prevTasks => {
-      const updatedTasks = prevTasks.map(task => {
-      return task.id === id ? {...task, isComplete: !task.isComplete} : task;
+  React.useEffect(() => {
+    axios.get('http://127.0.0.1:5000/tasks').then((resp) => {
+      // console.log(resp.data);
+      const newTaskData = resp.data.map((task) => {
+        return {
+          id: task.id,
+          title: task.title,
+          isComplete: task.is_complete
+        };
+      });
+      setTasks(newTaskData);
     });
-    return updatedTasks;
-  })
-);
+  }, []);
 
-  const removeTask = (id) => (
-    setTasks (prevTasks => {
-      const updatedTasks = prevTasks.filter(task => task.id !== id );
-      return updatedTasks;
-    })
-  );
+  // const setComplete = (id) => {
+  //   axios.patch(`http://127.0.0.1:5000/tasks/${id}/mark_complete`).then(() => {
+  //     setTasks(prevTasks => {
+  //       const updatedTasks = prevTasks.map(task => {
+  //       return task.id === id ? {...task, isComplete: !task.isComplete} : task;
+  //       });
+  //     return updatedTasks;
+  //     });
+  //   });
+  // };
+
+  const toggleComplete = (id) => {
+    console.log("id", id)
+    const updatedTasks = tasks.map(task => {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+        let routes = 'mark_complete';
+        
+        if (!task.isComplete) {
+          routes = 'mark_incomplete';
+        }
+          
+        axios.patch(`http://127.0.0.1:5000/tasks/${id}/${routes}`)
+          .then(() => {
+            setTasks(updatedTasks);
+          })
+          .catch((err) => console.log(err));
+      }
+      return task;
+    });
+  };
   
+
+
+  const removeTask = (id) => {
+    axios.delete(`http://127.0.0.1:5000/tasks/${id}`).then(() => {
+      setTasks (prevTasks => {
+        const updatedTasks = prevTasks.filter(task => task.id !== id );
+        return updatedTasks;
+      });
+    });
+  };
+  
+  // const addTask = (newTaskData) => {
+  //   axios.post(`http://localhost:5000/tasks`, newTaskData).then((resp)=>{
+  //     const newTasks = [...tasks];
+  //   })
+  // }
 
   
   return (
@@ -42,7 +77,7 @@ const App = () => {
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div><TaskList tasks={tasks} setComplete={setComplete} removeTask={removeTask} /></div>
+        <div><TaskList tasks={tasks} setComplete={toggleComplete} removeTask={removeTask} /></div>
       </main>
     </div>
   );
